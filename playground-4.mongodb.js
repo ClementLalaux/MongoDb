090f2faf1ecda87b15e2ccac275197c19999306c
Clement Lalaux <lalauxclement@gmail.com>
2023-05-31 10:35:50 +0200

    Exo 3 et 3bis

---
+++ playground-4.mongodb.js
@@ -0,0 +1,21 @@
+
+use('livre');
+
+//db.livre.find();
+
+// db.livre.insertOne({
+//     nom : "Macbook Pro",
+//     fabriquand : "Apple",
+//     prix : "11435.99",
+//     options : ["Intel Core i5","Retina Display","Long life battery"]
+// });
+
+//db.livre.find({type : "Book"});
+
+//db.livre.find({year : {$gte : 2011}});
+
+//db.livre.find({$and : [{ year : {$gte : 2014} },{type : "Book"}]});
+
+//db.livre.find({authors : "Toru Ishida"});
+
+//db.livre.distinct("authors");
---
+++ playground-5.mongodb.js
@@ -0,0 +1,45 @@
+use('info');
+
+// db.createCollection('produits');
+
+// db.produits.insertOne({
+//     nom:"Macbook Pro",
+//     fabriquant:"Apple",
+//     prix:11435.99,
+//     options:["Intel Core i5","Retina Display","Long life battery"]
+// });
+
+// db.produits.insertOne({
+//     nom:"Macbook Air",
+//     fabriquant:"Apple",
+//     prix:125794.73,
+//     ultrabook : true,
+//     options:["Intel Core i7","SSD","Long life battery"]
+// });
+
+// db.produits.insertOne({
+//     nom:"Thinkpad X230",
+//     fabriquant:"Lenovo",
+//     prix:114358.74,
+//     ultrabook : true,
+//     options:["Intel Core i5","SSD","Long life battery"]
+// });
+db.produits.find();
+
+//db.produits.find().limit(1);
+
+//db.produits.find({fabriquant : "Lenovo"},{"_id" : 1});
+
+//db.produits.find({prix : {$gte : 13723}});
+
+//db.produits.find({ultrabook : true}).limit(1);
+
+//db.produits.find({nom: {$regex : '.*Macbook.*', $options: "i"}});
+
+//db.produits.find({nom: {$regex : '^Macbook', $options: "i"}});
+
+//db.produits.deleteMany({fabriquant : "Apple"});
+
+//let id = db.produits.findOne({nom : "Thinkpad X230"},{"_id" : 1})._id.toString();
+
+//db.produits.deleteOne({_id : new ObjectId(id)});
